ee62b76cbbee88c34b64568ebab9f78e0b93fc93
ttatsato <t-satoh@alby.co.jp>
2020-01-11 10:53:59 +0900

    [fix] example echo because echo v3

--- examples/chat-echo/main.go
+++ examples/chat-echo/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
 	"gopkg.in/olahol/melody.v1"
 	"net/http"
@@ -16,18 +15,17 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		http.ServeFile(c.Response().(*standard.Response).ResponseWriter, c.Request().(*standard.Request).Request, "index.html")
+		http.ServeFile(c.Response().Writer, c.Request(), "index.html")
 		return nil
 	})
 
 	e.GET("/ws", func(c echo.Context) error {
-		m.HandleRequest(c.Response().(*standard.Response).ResponseWriter, c.Request().(*standard.Request).Request)
+		m.HandleRequest(c.Response().Writer, c.Request())
 		return nil
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
 		m.Broadcast(msg)
 	})
-
-	e.Run(standard.New(":5000"))
+	e.Logger.Fatal(e.Start(":5000"))
 }
