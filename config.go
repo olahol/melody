bd1a983661484eeb892aeb3b415af7bc6dc50c95
misaya <>
2023-08-22 14:35:59 +0800

    add Custom configuration

--- config.go
+++ config.go
@@ -2,6 +2,8 @@ package melody
 
 import "time"
 
+type Option func(config *Config)
+
 // Config melody configuration struct.
 type Config struct {
 	WriteWait         time.Duration // Milliseconds until write times out.
@@ -18,5 +20,40 @@ func newConfig() *Config {
 		PingPeriod:        (60 * time.Second * 9) / 10,
 		MaxMessageSize:    512,
 		MessageBufferSize: 256,
+	}
+}
+
+// WithMaxMessageSize sets the write wait time.
+func WithMaxMessageSize(maxMessageSize int64) Option {
+	return func(c *Config) {
+		c.MaxMessageSize = maxMessageSize
+	}
+}
+
+// WithMessageBufferSize sets the message buffer size.
+func WithMessageBufferSize(messageBufferSize int) Option {
+	return func(c *Config) {
+		c.MessageBufferSize = messageBufferSize
+	}
+}
+
+// WithWriteWait sets the write wait time.
+func WithWriteWait(writeWait time.Duration) Option {
+	return func(c *Config) {
+		c.WriteWait = writeWait
+	}
+}
+
+// WithPongWait sets the pong wait time.
+func WithPongWait(pongWait time.Duration) Option {
+	return func(c *Config) {
+		c.PongWait = pongWait
+	}
+}
+
+// WithPingPeriod sets the ping period time.
+func WithPingPeriod(pingPeriod time.Duration) Option {
+	return func(c *Config) {
+		c.PingPeriod = pingPeriod
 	}
 }
